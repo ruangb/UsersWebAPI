158ddd0390eb8735bf91fce40dc99385a95552a0
Ruan <ruangbarros@gmail.com>
2021-03-17 21:23:03 -0300

    creating DbContext and repository

---
+++ UsersWebAPI/Repository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UsersWebAPI.Models;
+
+namespace UsersWebAPI.Repository
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+
+        IEnumerable<User> GetAll();
+
+        User Find(long id);
+
+        void Remove(long id);
+
+        void Update(User user);
+    }
+}
